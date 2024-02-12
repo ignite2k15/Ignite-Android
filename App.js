6f36da2549feea266daad9671319e91c0a2474fa
NimmanapalliHarika <nimmanapalliharika@gmail.com>
2024-02-12 15:26:20 +0530

    Added new file

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Started working </Text>
       <StatusBar style="auto" />
+      <Home/>
     </View>
   );
 }
